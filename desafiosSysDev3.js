8a8180fbdbeb3b317a0310395b4ed3116259e91c
brnowmar <bruno.mar.rj@gmail.com>
2023-01-10 17:06:05 -0300

    terminados

---
+++ desafiosSysDev3.js
@@ -0,0 +1,17 @@
+// ## isGreaterThan(a, b)
+
+// Returns true if `a` is greater than `b`
+
+// **Parameters**
+
+// **a**: number
+
+// **b**: number
+
+// **Returns**: boolean, `a` is larger than `b`
+
+function isGreaterThan(a, b) {
+  let result = a > b ? true : false;
+  return result;
+}
+console.log(isGreaterThan(2, 10))
